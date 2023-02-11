fa266399ab3d396b41be81e1ea106dda78c48481
Jack Sanders <jacklewissanders@gmail.com>
2023-02-11 17:10:12 -0600

    post

--- pages/api/venue/index.ts
+++ pages/api/venue/index.ts
@@ -1,11 +1,40 @@
+import HttpStatusCode from "@/types/status-codes";
 import { Venue } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+async function handleGET(res: NextApiResponse) {
+  try {
+    const venues = (await prisma?.venue.findMany()) || [];
+    if (!venues) res.status(HttpStatusCode.NOT_FOUND);
+    res.status(HttpStatusCode.OK).json(venues)
+  } catch (error) {
+    res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error)
+  }
+}
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const venues = (await prisma?.venue.findMany()) || [];
+    if (!venues) res.status(HttpStatusCode.NOT_FOUND);
+    res.status(HttpStatusCode.OK).json(venues)
+  } catch (error) {
+    res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(error)
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Venue[]>
 ) {
-  const venue = (await prisma?.venue.findMany()) || [];
-  res.status(200).json(venue);
-  console.log(req.method);
+  const { method } = req
+
+  switch (method) {
+    case 'GET':
+      return handleGET(res)
+      break
+    case 'POST':
+      return handlePOST(req, res)
+      break
+      console.log(req.method);
+  }
 }
